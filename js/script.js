1bc83c77e48ead989e88545a0b88a48668e80912
Em On <86396972+CoderArEmon@users.noreply.github.com>
2021-08-28 10:44:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$('.brand-slider').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+});
+
+$('.entertain-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+});
+
+$('.b-new').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+});
+
+$('.b-solution').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+});
+
+$('.b-customer').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+});
